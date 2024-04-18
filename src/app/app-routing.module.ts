89f96be39fcf69b2ac7bff9a80d876fe415e258f
gabri <gabriel.sek17@gmail.com>
2024-04-19 00:41:05 +0200

    routing to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  {path: '**', redirectTo: '/private-ijournal'}
+  {path: '**', component: AppComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
